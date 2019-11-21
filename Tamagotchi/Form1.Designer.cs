2d94153fddc0ea3dcca1ac93d90066acaf4d4a71
Leo <leo.hasselqvist@elev.ntig.se>
2019-11-21 13:04:53 +0100

    Tamagotchi Projekt av Teo & Leo

---
+++ Tamagotchi/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Tamagotchi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FeedButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.SleepButton = new System.Windows.Forms.Button();
+            this.HungerLabel = new System.Windows.Forms.Label();
+            this.HappyLabel = new System.Windows.Forms.Label();
+            this.EnergyLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FeedButton
+            // 
+            this.FeedButton.Location = new System.Drawing.Point(507, 509);
+            this.FeedButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FeedButton.Name = "FeedButton";
+            this.FeedButton.Size = new System.Drawing.Size(112, 83);
+            this.FeedButton.TabIndex = 0;
+            this.FeedButton.Text = "Feed";
+            this.FeedButton.UseVisualStyleBackColor = true;
+            this.FeedButton.Click += new System.EventHandler(this.FeedButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(290, 509);
+            this.PlayButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(112, 83);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // SleepButton
+            // 
+            this.SleepButton.Location = new System.Drawing.Point(705, 509);
+            this.SleepButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SleepButton.Name = "SleepButton";
+            this.SleepButton.Size = new System.Drawing.Size(114, 83);
+            this.SleepButton.TabIndex = 2;
+            this.SleepButton.Text = "Sleep";
+            this.SleepButton.UseVisualStyleBackColor = true;
+            this.SleepButton.Click += new System.EventHandler(this.SleepButton_Click);
+            // 
+            // HungerLabel
+            // 
+            this.HungerLabel.AutoSize = true;
+            this.HungerLabel.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HungerLabel.Location = new System.Drawing.Point(164, 125);
+            this.HungerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HungerLabel.Name = "HungerLabel";
+            this.HungerLabel.Size = new System.Drawing.Size(179, 61);
+            this.HungerLabel.TabIndex = 3;
+            this.HungerLabel.Text = "Food: 0";
+            // 
+            // HappyLabel
+            // 
+            this.HappyLabel.AutoSize = true;
+            this.HappyLabel.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HappyLabel.Location = new System.Drawing.Point(447, 125);
+            this.HappyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.HappyLabel.Name = "HappyLabel";
+            this.HappyLabel.Size = new System.Drawing.Size(286, 61);
+            this.HappyLabel.TabIndex = 4;
+            this.HappyLabel.Text = "Happiness: 0";
+            // 
+            // EnergyLabel
+            // 
+            this.EnergyLabel.AutoSize = true;
+            this.EnergyLabel.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnergyLabel.Location = new System.Drawing.Point(798, 125);
+            this.EnergyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EnergyLabel.Name = "EnergyLabel";
+            this.EnergyLabel.Size = new System.Drawing.Size(222, 61);
+            this.EnergyLabel.TabIndex = 5;
+            this.EnergyLabel.Text = "Energy: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(458, 212);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.EnergyLabel);
+            this.Controls.Add(this.HappyLabel);
+            this.Controls.Add(this.HungerLabel);
+            this.Controls.Add(this.SleepButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.FeedButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Tamagotchi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button FeedButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button SleepButton;
+        private System.Windows.Forms.Label HungerLabel;
+        private System.Windows.Forms.Label HappyLabel;
+        private System.Windows.Forms.Label EnergyLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Tamagotchi/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Alla knappar kallar varsin funktion
+
+namespace Tamagotchi
+{
+    public partial class Form1 : Form
+    {
+        int food = 100;
+        int happiness = 100;
+        int energy = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            timer2.Interval = 1000;
+            timer2.Start();
+        }
+
+        private void FeedButton_Click(object sender, EventArgs e)
+        {
+            // Funktion för att mata
+            food += 10;
+        }
+
+        private void SleepButton_Click(object sender, EventArgs e)
+        {
+            // Funktion för att sova
+            food -= 10;
+            energy += 10;
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            // Funktion för att leka
+            happiness += 10;
+            energy -= 10;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (food > 0 && happiness > 0 && energy > 0)
+            {
+                // Funktion för att uppdatera variablerna
+                HungerLabel.Text = "Food : " + food;
+                HappyLabel.Text = "Happiness : " + happiness;
+                EnergyLabel.Text = "Energy : " + energy;
+            }
+            else
+            {
+                // die
+                die();
+            }
+        
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            food -= 2;
+            happiness -= 1;
+        }
+
+
+        private void die()
+        {
+            HungerLabel.Text = "";
+            HappyLabel.Text = "YOU DIED GAME OVER";
+            EnergyLabel.Text = "";
+            FeedButton.Enabled = false;
+            PlayButton.Enabled = false;
+            SleepButton.Enabled = false;
+            timer2.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
